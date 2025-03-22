6e84535a71a2c3f67680d293803cf4b440114c54
Christopher Earl <386835+chrisearl@users.noreply.github.com>
2025-03-22 10:55:10 -0400

    Site updates

---
+++ assets/scripts/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+  console.info('DOM is fully loaded and ready');
+  init();
+});
+
+function init() {
+  console.info('Application initialized');
+}
